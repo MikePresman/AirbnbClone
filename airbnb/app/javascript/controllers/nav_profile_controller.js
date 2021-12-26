81aed0a8de55b189186f7114e9b70d43f6f316be
Mike Presman <Michael.Presman@ibm.com>
2021-12-26 00:48:51 -0500

    refactor

--- airbnb/app/javascript/controllers/nav_profile_controller.js
+++ airbnb/app/javascript/controllers/nav_profile_controller.js
@@ -1,20 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
-import { useClickOutside } from 'stimulus-use'
 
 // Connects to data-controller="profile"
 export default class extends Controller {
-  static values = {hidden: false}
 
   connect() {
-    useClickOutside(this);
   }
 
   dropdown(){
-    if (!this.hiddenValue){
-      document.getElementById("profile_nav-dropdown").classList.remove("hidden");
-    }else{
-      document.getElementById("profile_nav-dropdown").classList.add("hidden");
-    }
-    this.hiddenValue = !this.hiddenValue;
+    document.getElementById("profile_nav-dropdown").classList.toggle("hidden");
   }
 }
