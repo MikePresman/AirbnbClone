2be5c66c35f9aec73c47f94bab62f5a20ed28779
Mike Presman <mikepresman@gmail.com>
2021-12-27 17:43:04 -0500

    feat(auth): devise (#3)
    * devise auth
    
    * basic devise setup

--- airbnb/config/routes.rb
+++ airbnb/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "/login", to: "devise/sessions#new"
+    post "/login", to: "devise/sessions#create"
 
+    delete "/logout", to: "devise/sessions#destory"
+
+    get "/register", to: "devise/registrations#new"
+    post "/register", to: "devise/registrations#create"
+  end
+
+  devise_for :users
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
