9d2cd969358566853ec2665e0e021f7835e7d0b4
Mike Presman <Michael.Presman@ibm.com>
2021-12-27 20:29:45 -0500

    city display image column

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_012554) do
+ActiveRecord::Schema.define(version: 2021_12_28_012834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_012554) do
     t.string "country", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "display_image"
   end
 
   create_table "rents", force: :cascade do |t|
