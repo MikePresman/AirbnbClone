c618d9a70c52ef29fe29a8eff253c27633512cd2
Mike Presman <Michael.Presman@ibm.com>
2021-12-27 20:35:52 -0500

    admin table

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_012834) do
+ActiveRecord::Schema.define(version: 2021_12_28_013252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_admins_on_user_id"
+  end
 
   create_table "city", force: :cascade do |t|
     t.string "city_name", null: false
@@ -47,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_012834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "admins", "users"
   add_foreign_key "rents", "city"
   add_foreign_key "rents", "users"
 end
