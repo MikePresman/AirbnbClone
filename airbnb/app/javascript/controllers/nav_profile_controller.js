04393c644ef54b851ae722e97647e3726c06d517
Mike Presman <Michael.Presman@ibm.com>
2021-12-27 19:27:24 -0500

    login and registration wip

--- airbnb/app/javascript/controllers/nav_profile_controller.js
+++ airbnb/app/javascript/controllers/nav_profile_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="profile"
+// Connects to data-controller="nav-profile"
 export default class extends Controller {
 
   connect() {
@@ -8,5 +8,9 @@ export default class extends Controller {
 
   dropdown(){
     document.getElementById("profile_nav-dropdown").classList.toggle("hidden");
+  }
+
+  login(){
+    document.getElementById("login-modal").classList.toggle("hidden");
   }
 }
