79aeadd2af4859833bdd85453daad6988b2c7f7b
Mike Presman <Michael.Presman@ibm.com>
2022-01-02 00:33:04 -0500

    fix form_with model

--- airbnb/app/models/city.rb
+++ airbnb/app/models/city.rb
@@ -1,4 +1,13 @@
 class City < ApplicationRecord
   belongs_to :rent
-  #validate the city is unique for the given country
+
+  validates :city_name, :presence => true
+  validates :country, :presence => true
+
+  validate :entry_must_be_unique
+
+  def entry_must_be_unique
+    !City.where(city_name: city_name, country: country).exists?
+  end
+
 end
