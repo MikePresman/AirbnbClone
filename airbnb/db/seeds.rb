0e6328ce57d586c12f6cdf77ec1190c0f150ca2c
mike <mikepresman@gmail.com>
2022-01-27 21:45:57 -0500

    turbo install + enable redis via docker-compose

--- airbnb/db/seeds.rb
+++ airbnb/db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#mikepresman@gmail.com -> Apple1234
     City.create(city_name: "New York City", country: "United States", display_image: "https://a0.muscache.com/im/pictures/3d3a87bc-9a2c-47b7-8a36-22969be34e5d.jpg?im_w=720")    
     City.create(city_name: "San Fransciso", country: "United States", display_image: "https://www.fodors.com/assets/destinations/54494/alamo-square-san-francisco-california-usa_980x650.jpg")    
     City.create(city_name: "Malibu", country: "United States", display_image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRwSYLWzJjA7bUryfvQlvrQtJtT1z7dwQ7CULY4RcPvED_2iUm1voCKROx5QIJXUoiADZTspTFB92iB0g")    
