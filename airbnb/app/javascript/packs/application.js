03d88f5203f2a31ca1282ed9e621c7b7ac43d940
mike <mikepresman@gmail.com>
2022-02-01 22:25:32 -0500

    removed turbo-links garbage

--- airbnb/app/javascript/packs/application.js
+++ airbnb/app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../css/application.css'
@@ -12,5 +11,4 @@ import "../controllers"
 import "@hotwired/turbo-rails"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
