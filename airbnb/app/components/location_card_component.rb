797c9f133bf2a273ba865954452489f4ff057f97
mike <mikepresman@gmail.com>
2022-02-18 23:16:04 -0500

    added register modal

--- airbnb/app/components/location_card_component.rb
+++ airbnb/app/components/location_card_component.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
 class LocationCardComponent < ViewComponent::Base
-  def initialize(obj:, name:, image:)
+  def initialize(obj:, name:, image:, link:)
     @obj = obj
     @name = name
     @image = image
   end
   
-  #TODO Finish login/signup modal
-  #TODO cleanup stimulus controllers
-  #TODO Testing
-  #TODO figure out helper for passing url so we can use this card much more abstractly
-  #TODO Ransack
-  #TODO add pundit or rollify
-  #TODO Flipper
-  #TODO Ahoy gem
+  # TODO figure out helper for passing url so we can use this card much more abstractly
+  # TODO Testing -> RSpec
+  # TODO Ransack
+  # TODO Rolify 
+  # TODO Flipper
+  # TODO Pre-commit. Secrets and rubocop
+  # TODO Ahoy gem
 
   #Add rental
   #add experiences
   #add online etc.
-  
 
 
 end
--- airbnb/config/routes.rb
+++ airbnb/config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   root "home#index"
 
   devise_scope :user do
-    get "/login", to: "devise/sessions#new"
     post "/login", to: "devise/sessions#create"
-
     delete "/users/logout", to: "devise/sessions#destory"
-
-    get "/register", to: "devise/registrations#new"
     post "/register", to: "devise/registrations#create"
   end
   
