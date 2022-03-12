487ffc3ea38760605b8eead40617139be984375a
mike <mikepresman@gmail.com>
2022-03-11 23:38:30 -0500

    removed alpine2 need using alpinejs3 cdn

--- airbnb/app/javascript/packs/application.js
+++ airbnb/app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../controllers"
-import 'alpinejs'
 import '../css/application.css'
 
 Rails.start()
