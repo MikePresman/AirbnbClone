0ab745f41295047666817f1d7fe3b32e88bb608f
mike <mikepresman@gmail.com>
2022-03-25 23:52:29 -0400

    export env var in makefile

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_035949) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_01_035949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_admins_on_user_id"
   end
 
   create_table "cities", force: :cascade do |t|
     t.string "city_name", null: false
     t.string "country", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "display_image"
   end
 
@@ -36,8 +35,8 @@ ActiveRecord::Schema.define(version: 2022_01_01_035949) do
     t.integer "age"
     t.date "date_of_birth"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -47,8 +46,8 @@ ActiveRecord::Schema.define(version: 2022_01_01_035949) do
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.bigint "cities_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["cities_id"], name: "index_rents_on_cities_id"
     t.index ["user_id"], name: "index_rents_on_user_id"
   end
@@ -57,10 +56,10 @@ ActiveRecord::Schema.define(version: 2022_01_01_035949) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
