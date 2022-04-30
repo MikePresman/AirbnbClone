6b5fb830b49dd0a7a9834120d6420182efc714e2
mike <mikepresman@gmail.com>
2022-04-30 17:10:10 -0400

    init

--- airbnb/db/seeds.rb
+++ airbnb/db/seeds.rb
@@ -12,4 +12,4 @@
     City.create(city_name: "Malibu", country: "United States", display_image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRwSYLWzJjA7bUryfvQlvrQtJtT1z7dwQ7CULY4RcPvED_2iUm1voCKROx5QIJXUoiADZTspTFB92iB0g")    
     City.create(city_name: "Austin", country: "United States", display_image: "http://res.cloudinary.com/simpleview/image/upload/v1601501261/clients/austin/Barton_Springs_Pool_Views_Credit_Ryan_Kyte_Lifetime_Usage_f1c319f3-478e-4a04-ae35-613dfb525d0a.jpg")    
     City.create(city_name: "Miami", country: "United States", display_image: "https://a.cdn-hotels.com/gdcs/production105/d845/998803d7-b594-4416-8800-8d73cb4e1985.jpg")    
-    
+    User.create(email: "admin@g.com", password: "password1", password_confirmation: "password1")
